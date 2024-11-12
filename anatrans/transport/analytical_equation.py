Implemented superposition calculation and fixed various bugs in analyitical solution

anatrans/transport/analytical_equation.py
@@ -17,6 +17,7 @@ class Transport:
                  dx : float = None,
                  dy : float = None,
                  dt : float = None,
+                 initial : str = "direct",
                  verbose : bool = True
                  ) -> None:
         """Initialise the class, set up dimensions and output array."""
@@ -33,12 +34,17 @@ class Transport:
         if not val_success:
             raise ValueError("Not all required input values are of the expected value or data type.")
 
-        self.R = calculate_retardation(self.pars)
-        self.v = calculate_flow_velocity(self.pars) / self.R
-        self.ax, self.ay, self.az = calculate_dispersivity(self.pars)
-
         self.source_y = self.pars["c_source"][:,0]
         self.source_c = self.pars["c_source"][:,1]
+
+        self.R = calculate_retardation(self.pars)
+        self.v = calculate_flow_velocity(self.pars)
+
+        self.source_decay()
+
+        self.v = self.v / self.R
+        self.ax, self.ay, self.az = calculate_dispersivity(self.pars)
+
         self.width = np.max(self.source_y) * 2
 
         # If dx, dy or dt are not given, determine a proper value based on modeled area dimensions.
@@ -48,7 +54,96 @@ class Transport:
         self.dt = dt
 
         self.x = np.arange(0, parameters["l_model"], self.dx)
+
+        if initial == "direct":
+            self.initial_direct()
+        elif initial == "superposition":
+            self.initial_superposition()
+        else:
+            print("initial method is unknown, defaulting to direct")
+            self.initial_direct()
+
+
         # Center y dimension on the centerline of the plume
+        # self.y = np.zeros(2 * len(self.source_y) - 1)
+        # self.y[0:len(self.source_y)] = -self.source_y[::-1]
+        # self.y[len(self.source_y) - 1:] = self.source_y
+        #
+        # self.c0 = np.zeros(2 * len(self.source_c) - 1)
+        # self.c0[0:len(self.source_c)] = self.source_c[::-1]
+        # self.c0[len(self.source_c) - 1:] = self.source_c
+
+        #self.y = np.arange(0, self.width, self.dy) - (self.width / 2)
+        self.t = np.arange(0.00001, parameters["t_model"] + 0.00001 + self.dt, self.dt)
+        #self.t[0] = self.t[0] + 0.9
+        print(self.t[0] + 0.9)
+        self.xxx = np.tile(self.x, (len(self.t), len(self.y), 1))
+        self.yyy = np.tile(self.y[:, None], (len(self.t), 1, len(self.x)))
+
+        self.ttt = np.tile(self.t[:, None, None], (1, len(self.y), len(self.x)))
+
+        if self.c0.ndim == 2:
+            self.ccc0_list = [0] * len(self.c0[0,:])
+            print(len(self.ccc0_list))
+            print(self.c0[:,0])
+            for i in range(len(self.ccc0_list)):
+                ccc0 = np.tile(self.c0[:, i][:, None], (len(self.t), 1, len(self.x)))
+                self.ccc0_list[i] = ccc0
+            print(ccc0.shape)
+        elif self.c0.ndim == 1:
+            self.ccc0 = np.tile(self.c0[:, None], (len(self.t), 1, len(self.x)))
+        else:
+            print("something went very wrong")
+        self.cxyt = np.zeros(self.xxx.shape)
+
+    def no_decay(self):
+        """Calculate the Domenico analytical model without decay."""
+
+        inf_erfc_x = (self.xxx - self.v * self.ttt) / (2 * np.sqrt(self.ax * self.v * self.ttt))
+        erfc_x = erfc(inf_erfc_x)
+
+        erf_z = (erf(self.pars["d_source"] / (2 * np.sqrt(self.az * self.xxx)))
+                 - erf(-self.pars["d_source"] / (2 * np.sqrt(self.az * self.xxx))))
+
+        exp_source = np.exp(-self.k_source * (self.ttt - self.xxx / self.v))
+        exp_source = np.where(exp_source > 1, 1, exp_source)
+
+        if self.c0.ndim == 2:
+            ccc0_source_list = [0] * len(self.ccc0_list)
+            for i in range(len(self.c0[0,:])):
+                erf_y = (erf(((self.source_y[i+1] * 2) / 2) / (2 * np.sqrt(self.ay * self.xxx)))
+                         - erf((-(self.source_y[i+1] * 2) / 2) / (2 * np.sqrt(self.ay * self.xxx))))
+
+                ccc0_source_list[i] = self.ccc0_list[i] * exp_source
+                cxyt = 1 / 8 * ccc0_source_list[i] * erfc_x * erf_y * erf_z
+                self.cxyt += cxyt
+            ccc0_array = np.asarray(ccc0_source_list)
+        elif self.c0.ndim == 1:
+            erf_y = (erf((self.yyy + self.width / 2) / (2 * np.sqrt(self.ay * self.xxx)))
+                     - erf((self.yyy - self.width / 2) / (2 * np.sqrt(self.ay * self.xxx))))
+            self.cxyt = 1 / 8 * self.ccc0 * exp_source * erfc_x * erf_y * erf_z
+
+        else:
+            print("something went very wrong")
+
+        return self.cxyt, self.x, self.y, self.t, erfc_x
+
+    def initial_superposition(self):
+        """Function that calculates initial condition as a superposition of individual plumes varying in width
+         with constant concentration.
+        """
+        self.y = np.arange(-self.source_y[-1], self.source_y[-1] + self.dy, self.dy)
+
+        self.c0 = np.zeros((len(self.y), len(self.source_c) - 1))
+        for i in range(len(self.source_c) - 1):
+            y_value = self.source_y[i + 1]
+            con_value = self.source_c[i] - self.source_c[i+1]
+            y_pos_up = (np.abs(self.y - y_value)).argmin()
+            y_pos_down = (np.abs(self.y + y_value)).argmin()
+            self.c0[y_pos_down:y_pos_up+1, i] = con_value
+
+    def initial_direct(self):
+        """Function that calculates initial condition as a plume with varying concentration across its width."""
         self.y = np.zeros(2 * len(self.source_y) - 1)
         self.y[0:len(self.source_y)] = -self.source_y[::-1]
         self.y[len(self.source_y) - 1:] = self.source_y
@@ -57,31 +152,21 @@ class Transport:
         self.c0[0:len(self.source_c)] = self.source_c[::-1]
         self.c0[len(self.source_c) - 1:] = self.source_c
 
-        #self.y = np.arange(0, self.width, self.dy) - (self.width / 2)
-        self.t = np.arange(0.00001, parameters["t_model"] + 0.00001, self.dt)
-        #self.t[0] = self.t[0] + 0.9
-        print(self.t[0] + 0.9)
-        self.xxx = np.tile(self.x, (len(self.t), len(self.y), 1))
-        self.yyy = np.tile(self.y[:, None], (len(self.t), 1, len(self.x)))
-        self.ccc0 = np.tile(self.c0[:, None], (len(self.t), 1, len(self.x)))
-        self.ttt = np.tile(self.t[:, None, None], (1, len(self.y), len(self.x)))
+    def source_decay(self):
+        """Function that calculates the source zone decay constant."""
+        Q = self.v * self.pars["n"] * self.pars["d_source"] * np.max(self.source_y) * 2 * 1000
 
-        self.cxyt = np.zeros(self.xxx.shape)
+        C0_avg = 0
+        for i in range(len(self.source_y) - 1):
+            if i == 0:
+                yc = self.source_y[i + 1] * self.source_c[i] * 2
+            else:
+                yc = (self.source_y[i+1] - self.source_y[i]) * self.source_c[i] * 2
+            C0_avg += yc
+        C0_avg = C0_avg / (np.max(self.source_y) * 2)
 
-    def no_decay(self):
-        """Calculate the Domenico analytical model without decay."""
-        inf_erfc_x = (self.xxx - self.v * self.ttt) / (2 * np.sqrt(self.ax * self.v * self.ttt))
-        erfc_x = erfc(inf_erfc_x)
+        self.k_source = Q * C0_avg / (self.pars["m_total"] * 1e6)
 
-        erf_y = (erf((self.yyy + self.width / 2) / (2 * np.sqrt(self.ay * self.xxx)))
-                 - erf((self.yyy - self.width / 2) / (2 * np.sqrt(self.ay * self.xxx))))
-
-        erf_z = (erf(self.pars["d_source"] / (2 * np.sqrt(self.az * self.xxx)))
-                 - erf(-self.pars["d_source"] / (2 * np.sqrt(self.ay * self.xxx))))
-
-        cxty = 1/8 * self.ccc0 * erfc_x * erf_y * erf_z
-
-        return cxty, self.x, self.y, self.t
 
     def visualize_initial(self):
         """Temporary function to check if initial condition is correct."""
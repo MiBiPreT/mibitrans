b834d7972daf1ac487854f9db2eaff7930e84546
Alraune Zech <a.zech@uu.nl>
2025-07-01 17:16:15 +0200

    update init with aliases for top level function import

--- mibitrans/__init__.py
+++ mibitrans/__init__.py
@@ -6,3 +6,18 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 __author__ = "Alraune Zech"
 __email__ = "a.zech@uu.nl"
 __version__ = "0.3.0"
+
+# Add some commonly used functions as top-level imports
+
+from mibitrans.analysis.mass_balance import MassBalance
+from mibitrans.data.check_input import CheckInput
+from mibitrans.data.read import from_dict
+from mibitrans.transport.analytical_equation import Transport
+from mibitrans.visualize.plot_line import Lineplot
+from mibitrans.visualize.plot_surface import Plume
+from mibitrans.visualize.show_mass_balance import generate_mass_balance_tables
+from mibitrans.visualize.show_mass_balance import visualize_mass_balance
+
+
+
+

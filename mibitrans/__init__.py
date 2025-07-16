8335b1bc48beccd4d24d77e9a3edf46af0dc184a
raar1 <raarichardson@gmail.com>
2025-07-16 14:52:53 +0200

    Move module level imports to top

--- mibitrans/__init__.py
+++ mibitrans/__init__.py
@@ -1,14 +1,6 @@
 """Documentation about mibitrans."""
-import logging
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-__author__ = "Alraune Zech"
-__email__ = "a.zech@uu.nl"
-__version__ = "0.3.0"
 
 # Add some commonly used functions as top-level imports
-
 from mibitrans.analysis.mass_balance import MassBalance
 from mibitrans.data.check_input import CheckInput
 from mibitrans.data.read import from_dict
@@ -18,6 +10,10 @@ from mibitrans.visualize.plot_surface import Plume
 from mibitrans.visualize.show_mass_balance import generate_mass_balance_tables
 from mibitrans.visualize.show_mass_balance import visualize_mass_balance
 
+import logging
 
+logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-
+__author__ = "Alraune Zech"
+__email__ = "a.zech@uu.nl"
+__version__ = "0.3.0"

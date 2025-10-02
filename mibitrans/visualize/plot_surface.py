62741b64e261a3e858ec80be210151022645f380
Jorrit Bakker <135327417+JoYvBa@users.noreply.github.com>
2025-10-02 14:51:47 +0200

    Added animation to surface plots, expanded tests
    plume_2d and plume_3d functions are now able to make animations of concentration distribution. Visualization tests expanded with check if FuncAnimation object is generated.

--- mibitrans/visualize/plot_surface.py
+++ mibitrans/visualize/plot_surface.py
@@ -1,34 +1,55 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import animation
 from mibitrans.data.check_input import _check_model_type
 from mibitrans.data.check_input import _time_check
 from mibitrans.transport.domenico import Domenico
 
 
-def plume_2d(model, time=None, **kwargs):
+def plume_2d(model, time=None, animate=False, **kwargs):
     """Plot contaminant plume as a 2D colormesh, at a specified time.
 
     Args:
         model : Model object from mibitrans.transport.
-        time (float): Point of time for the plot. By default, last point in time is plotted.
+        time (float): Point of time for the plot. Will show the closest time step to given value.
+            By default, last point in time is plotted.
+        animate (bool): If True, animation of contaminant plume until given time is shown.
         **kwargs : Arguments to be passed to plt.pcolormesh().
 
     Returns a matrix plot of the input plume as object.
     """
     _check_model_type(model, Domenico)
     t_pos = _time_check(model, time)
-    plt.pcolormesh(model.x, model.y, model.cxyt[t_pos, :, :], **kwargs)
+    if not animate:
+        plt.pcolormesh(model.x, model.y, model.cxyt[t_pos, :, :], **kwargs)
+        plt.xlabel("Distance from source (m)")
+        plt.ylabel("Distance from plume center (m)")
+        plt.colorbar(label=r"Concentration (g/$m^{3}$)")
+    else:
+        fig, ax = plt.subplots()
+        mesh = ax.pcolormesh(model.x, model.y, model.cxyt[0, :, :], vmin=0, vmax=np.max(model.cxyt), **kwargs)
+        cbar = fig.colorbar(mesh, ax=ax)
+        cbar.set_label("Concentration (C/C0)")
+        ax.set_xlabel("Distance from source (m)")
+        ax.set_ylabel("Distance from plume center (m)")
 
-    plt.xlabel("Distance from source (m)")
-    plt.ylabel("Distance from plume center (m)")
-    plt.colorbar(label=r"Concentration (g/$m^{3}$)")
+        def update(frame):
+            mesh.set_array(model.cxyt[frame, :, :])
+            ax.set_title(f"Concentration distribution at t={model.t[frame]} days")
+            return mesh
+
+        ani = animation.FuncAnimation(fig=fig, func=update, frames=len(model.t))
+        return ani
 
 
-def plume_3d(model, time=None, **kwargs):
+def plume_3d(model, time=None, animate=False, **kwargs):
     """Plot contaminant plume as a 3D surface, at a specified time.
 
     Args:
         model : Model object from mibitrans.transport.
-        time (float): Point of time for the plot. By default, last point in time is plotted.
+        time (float): Point of time for the plot. Will show the closest time step to given value.
+            By default, last point in time is plotted.
+        animate (bool): If True, animation of contaminant plume until given time is shown.
         **kwargs : Arguments to be passed to plt.plot_surface().
 
     Returns:
@@ -37,12 +58,45 @@ def plume_3d(model, time=None, **kwargs):
     _check_model_type(model, Domenico)
     t_pos = _time_check(model, time)
 
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    ax.plot_surface(model.xxx[t_pos, :, :], model.yyy[t_pos, :, :], model.cxyt[t_pos, :, :], **kwargs)
+    if not animate:
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        surf = ax.plot_surface(model.xxx[t_pos, :, :], model.yyy[t_pos, :, :], model.cxyt[t_pos, :, :], **kwargs)
+        ax.view_init(elev=30, azim=310)
+        ax.set_xlabel("Distance from source (m)")
+        ax.set_ylabel("Distance from plume center (m)")
+        ax.set_zlabel(r"Concentration [$g/m^{3}$]")
+        return ax
 
-    ax.view_init(elev=30, azim=310)
-    ax.set_xlabel("Distance from source (m)")
-    ax.set_ylabel("Distance from plume center (m)")
-    ax.set_zlabel(r"Concentration [$g/m^{3}$]")
+    else:
+        if "cmap" not in kwargs and "color" not in kwargs:
+            kwargs["color"] = "tab:blue"
+        model_max = np.max(model.cxyt)
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        surface = ax.plot_surface(
+            model.xxx[0, :, :],
+            model.yyy[0, :, :],
+            model.cxyt[0, :, :],
+            vmin=0,
+            vmax=model_max,
+            **kwargs,
+        )
+        ax.set_xlabel("Distance from source (m)")
+        ax.set_ylabel("Distance from plume center (m)")
 
-    return ax
+        # plot_surface creates a static surface; need to create new plot every time step
+        def update(frame):
+            nonlocal surface
+            surface.remove()
+            surface = ax.plot_surface(
+                model.xxx[frame, :, :],
+                model.yyy[frame, :, :],
+                model.cxyt[frame, :, :],
+                vmin=0,
+                vmax=model_max,
+                **kwargs,
+            )
+            ax.set_title(f"Concentration distribution at t={model.t[frame]} days")
+            return surface
+
+        ani = animation.FuncAnimation(fig=fig, func=update, frames=len(model.t))
+        return ani
--- mibitrans/visualize/show_conditions.py
+++ mibitrans/visualize/show_conditions.py
@@ -2,6 +2,7 @@
 
 Module including various methods to visualize (input) parameter conditions, intended to only be called internally.
 """
+
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,13 +13,13 @@ def source_zone(source_parameters):
     source_y = source_parameters.source_zone_boundary
     source_c = source_parameters.source_zone_concentration
 
-    y_discretization = np.linspace(-source_y[-1] -source_y[-1]/10, source_y[-1] + source_y[-1]/10, 10000)
+    y_discretization = np.linspace(-source_y[-1] - source_y[-1] / 10, source_y[-1] + source_y[-1] / 10, 10000)
     c_values = np.zeros(len(y_discretization))
     for i, y in enumerate(source_y[::-1]):
-        c_values = np.where((y_discretization <= y) & (y_discretization >= -y), source_c[-(i+1)], c_values)
+        c_values = np.where((y_discretization <= y) & (y_discretization >= -y), source_c[-(i + 1)], c_values)
 
-    indexer = np.linspace(1,0.3,len(source_y))
-    colormap = matplotlib.colormaps['YlGnBu']
+    indexer = np.linspace(1, 0.3, len(source_y))
+    colormap = matplotlib.colormaps["YlGnBu"]
 
     plt.figure(dpi=300)
 
@@ -29,23 +30,24 @@ def source_zone(source_parameters):
                 x1=c_values,
                 where=(y_discretization <= y) & (y_discretization >= -y),
                 color=colormap(indexer[i]),
-                zorder=len(source_y) + 2
+                zorder=len(source_y) + 2,
             )
         else:
             plt.fill_betweenx(
                 y=y_discretization,
                 x1=c_values,
-                #Boolean array for domain of source zone i
-                where=((y_discretization <= y) & (y_discretization > source_y[i-1]))
-                      | ((y_discretization >= -y) & (y_discretization < -source_y[i-1])),
+                # Boolean array for domain of source zone i
+                where=((y_discretization <= y) & (y_discretization > source_y[i - 1]))
+                | ((y_discretization >= -y) & (y_discretization < -source_y[i - 1])),
                 color=colormap(indexer[i]),
-                zorder=len(source_y) + 2 - i
+                zorder=len(source_y) + 2 - i,
             )
 
     plt.xlabel(r"Source zone concentration $g/m^3$")
     plt.ylabel("Source zone y-coordinate")
     plt.title("Concentration distribution in the source zone")
 
+
 def model_grid(model_parameters):
     """Visualize the model grid."""
-    model_parameters = None
+    return None

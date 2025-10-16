0a964aec133d00d9bd7c5de26e5c3438fe06739d
Jorrit Bakker <135327417+JoYvBa@users.noreply.github.com>
2025-10-16 14:56:53 +0200

    Fixed linting, removed duplications

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,11 +11,13 @@ from mibitrans.transport import karanovic as kar
 
 @pytest.fixture(scope="session")
 def test_hydro_pars():
+    """HydrologicalParameters fixture with example data for tests."""
     return HydrologicalParameters(velocity=10 / 365, porosity=0.25, alpha_x=10, alpha_y=1, alpha_z=0.1)
 
 
 @pytest.fixture(scope="session")
 def test_att_pars():
+    """AttenuationParameters fixture with example data for tests."""
     return AttenuationParameters(
         retardation=1,
         half_life=0.1 * 365,
@@ -29,6 +31,7 @@ def test_att_pars():
 
 @pytest.fixture(scope="session")
 def test_source_pars():
+    """SourceParameters fixture with example data for tests."""
     return SourceParameters(
         source_zone_boundary=np.array([5, 10, 15]),
         source_zone_concentration=np.array([10, 5, 2]),
@@ -39,11 +42,13 @@ def test_source_pars():
 
 @pytest.fixture(scope="session")
 def test_model_pars():
+    """ModelParameters fixture with example data for tests."""
     return ModelParameters(model_length=50, model_width=30, model_time=3 * 365, dx=10, dy=5, dt=1 * 365)
 
 
 @pytest.fixture(scope="session")
 def test_model_pars_short(test_source_pars, test_model_pars):
+    """Model Parameters fixture with smaller model width for testing."""
     short_model_pars = copy.copy(test_model_pars)
     short_model_pars.model_width = test_source_pars.source_zone_boundary[-1] - 1
     return short_model_pars
@@ -51,29 +56,35 @@ def test_model_pars_short(test_source_pars, test_model_pars):
 
 @pytest.fixture(scope="module")
 def test_domenico_nodecay_model(test_hydro_pars, test_att_pars, test_source_pars, test_model_pars):
+    """domenico.NoDecay fixture model object for testing."""
     return dom.NoDecay(test_hydro_pars, test_att_pars, test_source_pars, test_model_pars)
 
 
 @pytest.fixture(scope="module")
 def test_domenico_lineardecay_model(test_hydro_pars, test_att_pars, test_source_pars, test_model_pars):
+    """domenico.LinearDecay fixture model object for testing."""
     return dom.LinearDecay(test_hydro_pars, test_att_pars, test_source_pars, test_model_pars)
 
 
 @pytest.fixture(scope="module")
 def test_domenico_instantreaction_model(test_hydro_pars, test_att_pars, test_source_pars, test_model_pars):
+    """domenico.InstantReaction fixture model object for testing."""
     return dom.InstantReaction(test_hydro_pars, test_att_pars, test_source_pars, test_model_pars)
 
 
 @pytest.fixture(scope="session")
 def test_karanovic_nodecay_model(test_hydro_pars, test_att_pars, test_source_pars, test_model_pars):
+    """karanovic.NoDecay fixture model object for testing."""
     return kar.NoDecay(test_hydro_pars, test_att_pars, test_source_pars, test_model_pars)
 
 
 @pytest.fixture(scope="session")
 def test_karanovic_lineardecay_model(test_hydro_pars, test_att_pars, test_source_pars, test_model_pars):
+    """karanovic.LinearDecay fixture model object for testing."""
     return kar.LinearDecay(test_hydro_pars, test_att_pars, test_source_pars, test_model_pars)
 
 
 @pytest.fixture(scope="session")
 def test_karanovic_instantreaction_model(test_hydro_pars, test_att_pars, test_source_pars, test_model_pars):
+    """karanovic.InstantReaction fixture model object for testing."""
     return kar.InstantReaction(test_hydro_pars, test_att_pars, test_source_pars, test_model_pars)

b736d50359a8c9293b4c52890f73647de5c9cf6e
Bakker <j.y.bakker@uu.nl>
2026-03-26 14:01:28 +0100

    Update to sensitivity analysis models

--- examples/differences.py
+++ examples/differences.py
@@ -69,3 +69,37 @@ def mean_relative_difference(
     check_shape(cxyt_a, cxyt_b)
     cxyt_a_masked, cxyt_b_masked = mask(cxyt_a, cxyt_b, concentration_cutoff)
     return np.nanmean(relative_error(cxyt_a_masked, cxyt_b_masked), axis=mean_axis)
+
+def mean_sum_absolute_difference(
+    cxyt_a: np.ndarray, cxyt_b: np.ndarray, concentration_cutoff: float = 1e-5
+):
+    """Calculate the mean absolute difference of the sum of differences in the y-direction between two input arrays.
+
+    Args:
+        cxyt_a (np.ndarray) : Concentration array, must have same shape as cxyt_b.
+        cxyt_b (np.ndarray) : Concentration array, must have same shape as cxyt_a.
+        concentration_cutoff (float) : If both cxyt have concentrations below this cutoff value, they will not be taken
+            into account for calculating the average. If set to None, no cutoff value will be considered.
+            Default is 1e-5.
+    """
+    check_shape(cxyt_a, cxyt_b)
+    y_length = len(cxyt_a[0,:,0])
+    cxyt_a_center, cxyt_b_center = cxyt_a[:,y_length//2,:], cxyt_b[:,y_length//2,:]
+    mask = (cxyt_a_center >= concentration_cutoff) | (cxyt_b_center >= concentration_cutoff)
+    a, b = np.where(mask[:,None,:], cxyt_a, np.nan), np.where(mask[:,None,:], cxyt_b, np.nan)
+    error = absolute_error(a, b)
+    sum_error_xt = np.sum(error, axis=1)
+    mean_sum_error_t = np.nanmean(sum_error_xt, axis=1)
+    mean_sum_error = np.nanmean(mean_sum_error_t)
+    return mean_sum_error
+
+def rmse(
+    cxyt_a: np.ndarray,
+    cxyt_b: np.ndarray,
+    axis: int | tuple = (0, 1, 2),
+    concentration_cutoff: float = 1e-5,
+):
+    """Calculate the root-mean-square error between two input arrays."""
+    check_shape(cxyt_a, cxyt_b)
+    cxyt_a_masked, cxyt_b_masked = mask(cxyt_a, cxyt_b, concentration_cutoff)
+    return np.sqrt(np.nanmean((absolute_error(cxyt_a_masked, cxyt_b_masked)) ** 2, axis=axis))
